e6997f1392b484b9081a560d4c43de726a0e997e
pebencze <petra.bencze@deliveryhero.com>
2025-07-09 17:33:59 +0200

    feat(ResultEvaluator): add class to calc results

---
+++ src/main/kotlin/blackjack/model/ResultEvaluator.kt
@@ -0,0 +1,57 @@
+package blackjack.model
+
+data class DealerResult(var wins: Int = 0, var losses: Int = 0, var draws: Int = 0) {
+    override fun toString(): String {
+        return "Dealer: $wins wins, $losses losses, $draws draws"
+    }
+}
+data class PlayerResult(var win: Boolean = false, var draw: Boolean = false, val name: String = "") {
+    override fun toString(): String {
+        if (win) return "$name: win"
+        if (draw) return "$name: draw"
+        return "$name: loss"
+    }
+}
+
+class ResultEvaluator(val players: Players, val dealer: Dealer) {
+    var dealerResult: DealerResult = DealerResult()
+    var playerResults: List<PlayerResult> = listOf(PlayerResult())
+    init{
+        calculateDealerResults()
+    }
+
+    fun calculateDealerResults() {
+        if (dealer.isBusted()) {
+            // TODO: check players.players replace
+            dealerResult.losses = players.players.count() { !it.isBusted() }
+            return
+        }
+        players.forEach {
+            compareDealerToPlayer( it )
+        }
+    }
+
+    fun compareDealerToPlayer(player: Player) {
+        if (player.isBusted()){
+            dealerResult.wins++
+        }
+        if (player.isNotBusted()) {
+            when {
+                player.handCards.total < dealer.handCards.total -> dealerResult.wins++
+                player.handCards.total > dealer.handCards.total -> dealerResult.losses++
+                else -> dealerResult.draws++
+            }
+        }
+    }
+
+    //DEALER
+    // isNotBusted()
+    // -> compare against each player
+    // -> player.isBusted() -> win
+    // -> player.isNotBusted() -> compare totals (win or loss)
+    // isBusted() -> players.size * loss, 0 win
+    //PLAYERS
+    // isBusted() -> loss
+    // Dealer isNotBusted() -> compare (loss or win)
+    // Dealer isBusted() -> win
+}
