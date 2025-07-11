334db9d80bbce7462df1e402375af72ba0537c53
pebencze <petra.bencze@deliveryhero.com>
2025-07-11 14:47:46 +0200

    refactor(InputView): use require instead of if

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -7,18 +7,14 @@ object InputView {
     fun readNames(): List<String> {
         println("\nEnter the names of the players (comma-separated):")
         val input = readln()
-        if (input.isEmpty()) {
-            throw IllegalArgumentException(ErrorMessage.EMPTY_INPUT.toString())
-        }
+        require (input.isNotEmpty()) { (ErrorMessage.EMPTY_INPUT.toString()) }
         return input.split(",").map { it.trim() }
     }
 
     fun promptForDraw(player: Player): Boolean {
         println("\nWould ${player.name} like to draw another card? (y for yes, n for no)")
         val input = readln().trim()
-        if (input.isEmpty()) {
-            throw IllegalArgumentException(ErrorMessage.EMPTY_INPUT.toString())
-        }
+        require (input.isNotEmpty()) { (ErrorMessage.EMPTY_INPUT.toString()) }
         return when (input) {
             YES -> true
             NO -> false
