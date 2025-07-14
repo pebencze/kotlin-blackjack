cc03f0c6f1c476c39ea24303961f70acbfd31b92
pebencze <petra.bencze@deliveryhero.com>
2025-07-14 13:51:27 +0200

    feat(InitTest): test Init state

--- src/test/kotlin/blackjack/InitTest.kt
+++ src/test/kotlin/blackjack/InitTest.kt
@@ -1,27 +1,32 @@
 package blackjack
 
+import blackjack.model.State
 import blackjack.model.Init
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 class InitTest {
     @Test
+    fun `test empty hand`() {
+        var state: State = Init()
+        assertThat(state.hand.cards.size).isEqualTo(0)
+        assertThat(state).isInstanceOf(Init::class.java)
+    }
+
+    @Test
     fun `test drawing one card`() {
-        val init = Init()
-        init.draw()
-        asserThat(init.hand.cards.size).isEqualTo(1)
-        assertThat(init).isInstanceOf(Init::class.java)
+        var state: State = Init()
+        state = state.draw(ACE_DIAMONDS)
+        assertThat(state.hand.cards.size).isEqualTo(1)
+        assertThat(state).isInstanceOf(Init::class.java)
     }
 
-    fun `test drawing two cards`() {
-        val init = Init()
-        repeat(2) init.draw()
-        asserThat(init.hand.cards.size).isEqualTo(2)
-        assertThat(init).isInstanceOf(Init::class.java)
+    @Test
+    fun `test drawing two cards changes state`() {
+        var state: State = Init()
+        repeat(2) { state = state.draw(SIX_DIAMONDS) }
+        assertThat(state.hand.cards.size).isEqualTo(2)
+        assertThat(state).isNotInstanceOf(Init::class.java)
     }
 
-    fun `test drawing third card changes state to Hit`() {
-        val init = Init()
-        repeat(3) init.draw()
-        assertThat(init).isInstanceOf(Hit::class.java)
-        asserThat(init.hand.cards.size).isEqualTo(3)
-    }
 }
