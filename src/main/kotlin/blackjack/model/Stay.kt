37c832fb9a7c475ce7713f90fb105e4168e905c7
pebencze <petra.bencze@deliveryhero.com>
2025-07-14 15:56:25 +0200

    feat(StayTest, Stay): run and refactor all Stay tests and Stay class

--- src/main/kotlin/blackjack/model/Stay.kt
+++ src/main/kotlin/blackjack/model/Stay.kt
@@ -1,11 +1,14 @@
 package blackjack.model
 
 class Stay(override val hand: HandCards) : State {
+    init {
+        if (hand.size() < 2) { throw IllegalStateException() }
+    }
     override fun draw(card: Card): State {
         throw IllegalStateException()
     }
 
     override fun stay(): State {
-        return Stay(hand)
+        throw IllegalStateException()
     }
 }
---
+++ src/test/kotlin/blackjack/StayTest.kt
@@ -0,0 +1,46 @@
+package blackjack
+
+import blackjack.model.Stay
+import blackjack.model.HandCards
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class StayTest {
+    @ParameterizedTest
+    @MethodSource("invalidStayHands")
+    fun `test init Stay with wrong cards`(hand: HandCards){
+        assertThrows<IllegalStateException> { Stay(hand) }
+    }
+
+    @Test
+    fun `test init stay with correct cards`(){
+        val handCards = HandCards.from( QUEEN_DIAMONDS, QUEEN_SPADES)
+        assertDoesNotThrow { Stay(handCards) }
+    }
+
+    @Test
+    fun `test draw throws exception`(){
+        val bust = Stay(VALID_STAY_HAND)
+        assertThrows<IllegalStateException> { bust.draw(ACE_DIAMONDS) }
+    }
+
+    @Test
+    fun `test stay throws exception`(){
+        val bust = Stay(VALID_STAY_HAND)
+        assertThrows<IllegalStateException> { bust.stay() }
+    }
+
+    companion object {
+        @JvmStatic
+        fun invalidStayHands(): List<Arguments> = listOf(
+            Arguments.of(HandCards.from(ACE_DIAMONDS)),
+            Arguments.of(HandCards())
+        )
+
+        private val VALID_STAY_HAND = HandCards.from(ACE_DIAMONDS, FIVE_HEARTS)
+    }
+}
