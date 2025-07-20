d9f695891765225358981c0ea65b1db439ee50d0
pebencze <petra.bencze@deliveryhero.com>
2025-07-20 17:15:32 +0200

    refactor(Init): delete empty file

--- src/main/kotlin/blackjack/model/Init.kt
+++
@@ -1,8 +0,0 @@
-package blackjack.model.states
-
-//Q: Would it make sense to directly call draw twice in the init block when creating an Init object?
-//Q: Why do I not pass the whole deck as parameter to draw()?
-//Q: Is it not costly to always return new instances of State?
-//Q: Mutable handcards vs immutable handcards -> adding a new card always returns a new instance of HandCards
-// -> seems costly/inefficient to me, but of course it makes the logic more testable and
-
